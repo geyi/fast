f49eb8b48d4d926d3224fe479f92f199302366d6
geyi <geyichn@126.com>
2021-10-23 10:21:54 +0800

    test

---
+++ src/main/java/com/kuaidi100/fast/server/Constant.java
@@ -0,0 +1,6 @@
+package com.kuaidi100.fast.server;
+
+public class Constant {
+    public static final String BASE_PATH = "/Users/GEYI/tmp/";
+//    public static final String BASE_PATH = "C:\\Users\\kuaidi100\\Desktop\\";
+}
